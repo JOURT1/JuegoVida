8f82f042476fc6defaab15f76589c72a20589f64
JOURT1 <jhoelsuarez02@gmail.com>
2025-04-23 20:28:58 -0500

    Animacion player

---
+++ Assets/Assets/Scripts/NPCInteractuable.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class NPCInteractuable : MonoBehaviour
+{
+    public GameObject textoInteraccion;
+    public GameObject panelDialogo;
+    public TMP_Text textoDialogo;
+    public string pista = "El mar oculta más de lo que muestra, donde los mariscos son codiciados. Revisa el fondo...";
+    private Animator animator;
+    private bool jugadorCerca = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        textoInteraccion.SetActive(false);
+        panelDialogo.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (jugadorCerca && Input.GetKeyDown(KeyCode.E))
+        {
+            textoInteraccion.SetActive(false);
+            animator.SetTrigger("Hablar"); // tu animación debe tener este trigger
+            MostrarPista();
+            
+        }
+    }
+
+    void MostrarPista()
+    {
+        panelDialogo.SetActive(true);
+        textoDialogo.text = pista;
+        Invoke("OcultarPista", 5f);
+    }
+
+    void OcultarPista()
+    {
+        panelDialogo.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jugadorCerca = true;
+            textoInteraccion.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jugadorCerca = false;
+            textoInteraccion.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PLayerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayer : MonoBehaviour
+{
+    public float runspeed = 5;
+    public float mouseSensitivity = 500f;
+
+    public Animator animator;
+    public Transform cameraTransform;
+
+    private float x, y;
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //  Oculta el cursor
+    }
+
+    void Update()
+    {
+        //  Mouse Look
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Rotación vertical de la cámara (mirar arriba/abajo)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Rotación horizontal del personaje (girar con el mouse)
+        transform.Rotate(Vector3.up * mouseX);
+
+        //  Movimiento con teclado
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.forward * y + transform.right * x;
+        transform.Translate(moveDirection * runspeed * Time.deltaTime, Space.World);
+
+        //  Animación
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY", y);
+    }
+}
